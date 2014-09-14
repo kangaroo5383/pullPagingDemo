f93bf2cf88c77e3dcd38ec35469865dcc9727ad2
Jessica Lam <jessica@loungebuddy.com>
2014-09-13 22:58:12 -0700

    Initial commit

--- samplePully/ViewController.m
+++ samplePully/ViewController.m
@@ -9,7 +9,13 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *topOffsetConstraint;
+@property (weak, nonatomic) IBOutlet UIScrollView *topScrollView;
+@property (weak, nonatomic) IBOutlet UIScrollView *bottomScrollView;
 
+@end
+
+@interface ViewController (protocol) <UIScrollViewDelegate>
 @end
 
 @implementation ViewController
@@ -22,6 +28,29 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+@end
+
+@implementation ViewController (protocol)
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    CGFloat offset = scrollView.contentOffset.y;
+    BOOL shouldAnimate = NO;
+    if (scrollView == [self topScrollView] && offset > 145) {
+        [self topOffsetConstraint].constant = - ( CGRectGetHeight(scrollView.frame) + 16);
+        shouldAnimate = YES;
+    } else if (scrollView == [self bottomScrollView] && offset < -145) {
+        [self topOffsetConstraint].constant = -16;
+        shouldAnimate = YES;
+    }
+    if (shouldAnimate) {
+        [UIView animateWithDuration:0.33
+                              delay:0
+                            options:UIViewAnimationOptionCurveEaseInOut animations:^{
+                                [[self view] layoutSubviews];
+                            } completion:nil];
+    }
+
 }
 
 @end
